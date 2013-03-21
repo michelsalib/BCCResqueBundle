b39ed2ff5e82893cf02b3d3c705a4f20d75c2330
Kevin Saliou <kevin@saliou.name>
2013-03-21 12:42:31 +0100

    revert php-resque command call to /resque.php to be compatible with v1.2 of php-resque

--- Command/StartWorkerCommand.php
+++ Command/StartWorkerCommand.php
@@ -39,7 +39,7 @@ class StartWorkerCommand extends ContainerAwareCommand
         if (0 !== $m = (int) $input->getOption('memory-limit')) {
             $opt = sprintf('-d memory_limit=%dM', $m);
         }
-        $workerCommand = strtr('php %opt% %dir%/chrisboulton/php-resque/bin/resque', array(
+        $workerCommand = strtr('php %opt% %dir%/chrisboulton/php-resque/resque.php', array(
             '%opt%' => $opt,
             '%dir%' => $this->getContainer()->getParameter('bcc_resque.resque.vendor_dir'),
         ));
