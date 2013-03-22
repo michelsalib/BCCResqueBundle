a5214f4239a53db219153116c0a01521db1fbec2
Tobias Kluge <tobias@tobiaskluge.com>
2013-03-22 07:41:53 +0100

    Update StopScheduledWorkerCommand.php
    fallback if SIGTERM const is not defined

--- Command/StopScheduledWorkerCommand.php
+++ Command/StopScheduledWorkerCommand.php
@@ -10,6 +10,8 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
+if (!defined('SIGTERM')) define('SIGTERM', 15);
+
 class StopScheduledWorkerCommand extends ContainerAwareCommand
 {
     protected function configure()
