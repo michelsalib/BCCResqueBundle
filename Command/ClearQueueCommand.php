d510c961f4a269e2c54750eecf5c7d7cb214dec7
ulrik nielsen <ulrik@o3.dk>
2013-11-08 17:55:51 +0100

    clear out unused use statements

--- Command/ClearQueueCommand.php
+++ Command/ClearQueueCommand.php
@@ -3,7 +3,6 @@
 namespace BCC\ResqueBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use BCC\ResqueBundle\Resque;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
