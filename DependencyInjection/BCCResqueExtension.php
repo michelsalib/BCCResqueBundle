1934329b13b5857aaabc75e4672ddbaf8658233e
ulrik nielsen <ulrik@o3.dk>
2013-12-05 10:41:58 +0100

    Added configuration handeling for auto_retry and backoff strategy

--- DependencyInjection/BCCResqueExtension.php
+++ DependencyInjection/BCCResqueExtension.php
@@ -31,9 +31,21 @@ class BCCResqueExtension extends Extension
         $container->setParameter('bcc_resque.resque.redis.port', $config['redis']['port']);
         $container->setParameter('bcc_resque.resque.redis.database', $config['redis']['database']);
 
-        if(!empty($config['prefix'])) {
+        if (!empty($config['prefix'])) {
             $container->setParameter('bcc_resque.prefix', $config['prefix']);
             $container->getDefinition('bcc_resque.resque')->addMethodCall('setPrefix', array($config['prefix']));
+        }
+
+        if (!empty($config['auto_retry'])) {
+            if (isset($config['auto_retry'][0])) {
+                $container->getDefinition('bcc_resque.resque')->addMethodCall('setGlobalRetryStrategy', array($config['auto_retry'][0]));
+            } else {
+                if (isset($config['auto_retry']['default'])) {
+                    $container->getDefinition('bcc_resque.resque')->addMethodCall('setGlobalRetryStrategy', array($config['auto_retry']['default']));
+                    unset($config['auto_retry']['default']);
+                }
+                $container->getDefinition('bcc_resque.resque')->addMethodCall('setJobRetryStrategy', array($config['auto_retry']));
+            }
         }
     }
 }
